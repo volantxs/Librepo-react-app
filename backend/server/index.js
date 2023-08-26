6b7c858eafcd8d67841d7865fea8bcd3727d919c
volantxs <volantxs@gmail.com>
2023-08-26 22:44:02 +0530

    updated

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const User = require("../util/config.js");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
