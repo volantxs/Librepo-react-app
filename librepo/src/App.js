f5aca4bf0318d23515b352c2bdb8e9e1d1a5ff99
volantxs <volantxs@gmail.com>
2023-09-01 03:37:48 +0530

    updated

---
+++ librepo/src/App.js
@@ -0,0 +1,14 @@
+import logo from '../public/img/door.png';
+
+function App() {
+    return (
+        <div className="App">
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt="logo"/>
+
+            </header>
+        </div>
+    )
+}
+
+export default App;
