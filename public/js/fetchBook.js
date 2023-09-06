ff4cc3494b96b3e73b6ebb8ed134f8479c4e24fc
volantxs <volantxs@gmail.com>
2023-09-06 23:01:35 +0530

    updated thriller main page

--- public/js/fetchBook.js
+++ public/js/fetchBook.js
@@ -1,4 +1,19 @@
 
-var btn = document.getElementById('1')
+// var btn = document.getElementById('1');
 
-var modal = document.getElementById('bookModal')
+// var modal = document.getElementById('bookModal');
+
+// btn.onclick = function () {
+//     modal.style.display = 'flex';
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+//   }
+  
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   }
