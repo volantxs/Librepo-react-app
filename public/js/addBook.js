665a22aa79ed3ba11a0f987ea13049106526529c
volantxs <volantxs@gmail.com>
2023-09-27 16:10:05 +0530

    implemented import books functionality with google books API

--- public/js/addBook.js
+++ public/js/addBook.js
@@ -1,13 +1,39 @@
-const addBookBtn = document.getElementById("addBook");
-let themes = ['fright', 'wisdom', 'sorrow', 'joy']
-random_theme = themes[(Math.floor(Math.random() * themes.length))]
-var btnID = parseInt(localStorage.getItem("newBtnID")) ? parseInt(localStorage.getItem("newBtnID")) : 16 ; 
+// const addBookBtn = document.getElementById("addBook");
+// let themes = ['fright', 'wisdom', 'sorrow', 'joy']
+// random_theme = themes[(Math.floor(Math.random() * themes.length))]
+// var btnID = parseInt(localStorage.getItem("newBtnID")) ? parseInt(localStorage.getItem("newBtnID")) : 16 ; 
 
-function addBook(bookVaultIn, bookNameIn, btnID, bookPagesIn) {
-//     if (btnID > 15) {
-//     for (var i = 15; i < btnID; i++) {
-//         bookObj = JSON.parse(localStorage.getItem(btnID))
-//         let node = document.getElementById(bookObj.bookVault);
+// function addBook(bookVaultIn, bookNameIn, btnID, bookPagesIn) {
+// //     if (btnID > 15) {
+// //     for (var i = 15; i < btnID; i++) {
+// //         bookObj = JSON.parse(localStorage.getItem(btnID))
+// //         let node = document.getElementById(bookObj.bookVault);
+// //         // create div element
+// //         let newBook = document.createElement('div');
+// //         newBook.setAttribute('class', 'col-md-2 box p-2 m-3')
+// //         // create img element
+// //         let newBookImg = document.createElement('img')
+// //         newBookImg.setAttribute('class', 'img-fluid mx-auto d-block rounded m-3 img-shadow')
+// //         newBookImg.setAttribute('style', 'width: 100px; height: 160px;')
+// //         newBookImg.setAttribute('src', 'https://m.media-amazon.com/images/I/41wjnFxdxnL.jpg')
+// //         // create button element
+// //         let newBookBtn = document.createElement('button')
+// //         newBookBtn.setAttribute('class', 'btn btn-book')
+// //         newBookBtn.setAttribute('id', bookObj.btnID)
+// //         newBookBtn.setAttribute('onclick', "getXP(this.id)")
+// //         newBookBtn.setAttribute('value', bookObj.bookPagesIn)
+// //         newBookBtn.setAttribute('name', random_theme);
+// //         newBookBtn.innerHTML= bookObj.bookNameIn;
+// //         newBook.appendChild(newBookImg);
+// //         newBook.appendChild(newBookBtn);
+// //         try {
+// //             node.appendChild(newBook);
+// //         } catch(err) {
+// //             alert(err.message + "itsss hereee");
+// //         }
+// // }
+// // } else {
+//         let node = document.getElementById(bookVaultIn);
 //         // create div element
 //         let newBook = document.createElement('div');
 //         newBook.setAttribute('class', 'col-md-2 box p-2 m-3')
@@ -15,76 +41,50 @@ function addBook(bookVaultIn, bookNameIn, btnID, bookPagesIn) {
 //         let newBookImg = document.createElement('img')
 //         newBookImg.setAttribute('class', 'img-fluid mx-auto d-block rounded m-3 img-shadow')
 //         newBookImg.setAttribute('style', 'width: 100px; height: 160px;')
-//         newBookImg.setAttribute('src', 'https://m.media-amazon.com/images/I/41wjnFxdxnL.jpg')
+//         newBookImg.setAttribute('src', 'https://d1noes6pcxfkh4.cloudfront.net/97814746/9781474609883.jpg')
 //         // create button element
 //         let newBookBtn = document.createElement('button')
 //         newBookBtn.setAttribute('class', 'btn btn-book')
-//         newBookBtn.setAttribute('id', bookObj.btnID)
+//         // newBookBtn.setAttribute('style', 'width: 50px; height: 20px')
+//         newBookBtn.setAttribute('id', btnID)
 //         newBookBtn.setAttribute('onclick', "getXP(this.id)")
-//         newBookBtn.setAttribute('value', bookObj.bookPagesIn)
+//         newBookBtn.setAttribute('value', bookPagesIn)
 //         newBookBtn.setAttribute('name', random_theme);
-//         newBookBtn.innerHTML= bookObj.bookNameIn;
+//         newBookBtn.innerHTML = bookNameIn;
 //         newBook.appendChild(newBookImg);
 //         newBook.appendChild(newBookBtn);
 //         try {
 //             node.appendChild(newBook);
+           
 //         } catch(err) {
-//             alert(err.message + "itsss hereee");
+//             alert(err.message )+ "and this doomed ";
+//         }
+//         try {
+//                // alert(bookVaultIn)
+//                var bookObj = {
+//                 "bookVault": bookVaultIn, 
+//                 "bookNameIn": bookNameIn,
+//                 "bookPagesIn": bookPagesIn,
+//                 "btnID": btnID
+//             }
+//             localStorage.setItem(JSON.stringify(btnID), JSON.stringify(bookObj))
+
+//         } catch(err) {
+//             alert(err.message + "adding book obj");
+
 //         }
 // }
-// } else {
-        let node = document.getElementById(bookVaultIn);
-        // create div element
-        let newBook = document.createElement('div');
-        newBook.setAttribute('class', 'col-md-2 box p-2 m-3')
-        // create img element
-        let newBookImg = document.createElement('img')
-        newBookImg.setAttribute('class', 'img-fluid mx-auto d-block rounded m-3 img-shadow')
-        newBookImg.setAttribute('style', 'width: 100px; height: 160px;')
-        newBookImg.setAttribute('src', 'https://d1noes6pcxfkh4.cloudfront.net/97814746/9781474609883.jpg')
-        // create button element
-        let newBookBtn = document.createElement('button')
-        newBookBtn.setAttribute('class', 'btn btn-book')
-        // newBookBtn.setAttribute('style', 'width: 50px; height: 20px')
-        newBookBtn.setAttribute('id', btnID)
-        newBookBtn.setAttribute('onclick', "getXP(this.id)")
-        newBookBtn.setAttribute('value', bookPagesIn)
-        newBookBtn.setAttribute('name', random_theme);
-        newBookBtn.innerHTML = bookNameIn;
-        newBook.appendChild(newBookImg);
-        newBook.appendChild(newBookBtn);
-        try {
-            node.appendChild(newBook);
-           
-        } catch(err) {
-            alert(err.message )+ "and this doomed ";
-        }
-        try {
-               // alert(bookVaultIn)
-               var bookObj = {
-                "bookVault": bookVaultIn, 
-                "bookNameIn": bookNameIn,
-                "bookPagesIn": bookPagesIn,
-                "btnID": btnID
-            }
-            localStorage.setItem(JSON.stringify(btnID), JSON.stringify(bookObj))
 
-        } catch(err) {
-            alert(err.message + "adding book obj");
+// addBookBtn.addEventListener("click", function () {
+//     try {
+//         var bookVaultIn = document.getElementById("bookVault").value;
+//         var bookNameIn = document.getElementById("bookName").value;
+//         var bookPagesIn = document.getElementById("bookPages").value;
+//         addBook(bookVaultIn, bookNameIn, btnID, bookPagesIn);
+//         // btnID++
+//         // localStorage.setItem("newBtnID", JSON.stringify(btnID))
+//     } catch (err) {
+//         alert(err.message + "event listen");
+//     }
 
-        }
-}
-
-addBookBtn.addEventListener("click", function () {
-    try {
-        var bookVaultIn = document.getElementById("bookVault").value;
-        var bookNameIn = document.getElementById("bookName").value;
-        var bookPagesIn = document.getElementById("bookPages").value;
-        addBook(bookVaultIn, bookNameIn, btnID, bookPagesIn);
-        // btnID++
-        // localStorage.setItem("newBtnID", JSON.stringify(btnID))
-    } catch (err) {
-        alert(err.message + "event listen");
-    }
-
-})
+// })
--- public/js/modal.js
+++ public/js/modal.js
@@ -3,18 +3,55 @@ var importOpenBtn = document.getElementById("modalBtn");
 var importBookBtn = document.getElementById("addBook")
 var span = document.getElementsByClassName("close")[0];
 var searchInput = document.getElementById("searchBar")
+var importDiv = document.getElementById("importedBooks")
 var query = ''
 var URL = ''
+var googleKey = 'AIzaSyBmSE_tFFrraFhotpwPc8Vc4zMFEHL9zN8'
 
 importOpenBtn.onclick = function() {
   modal.style.display = "flex";
 }
 
-importBookBtn.onclick = () => {
+function ImportBook() {
   query = searchInput.value;
   searchInput.value = '';
-  URL = 'https://www.googleapis.com/books/v1/volumes?q=' + query;
+  URL = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&key=AIzaSyBmSE_tFFrraFhotpwPc8Vc4zMFEHL9zN8';
+  $.ajax({
+    url: URL.toString(),
+    dataType: 'json',
+    success: (data) => {
+      console.log(data);
+      var newBookNode = document.createElement('div')
+      newBookNode.className = "col-md-2 box p-2 m-3" 
+      // img element
+      var newBookImg = document.createElement('img')
+      newBookImg.src = data.items[0].volumeInfo.imageLinks.smallThumbnail;
+      console.log(data.items[0].volumeInfo.imageLinks.smallThumbnail)
+      newBookImg.className = "img-fluid mx-auto d-block rounded m-3 img-shadow"
+      newBookImg.style.width ="100px";
+      newBookImg.style.height ="160px";
+      newBookNode.appendChild(newBookImg);
+      // button element
+      var newBookBtn = document.createElement('button');
+      newBookBtn.className="btn btn-book"
+      newBookBtn.innerHTML = data.items[0].volumeInfo.title;
+      newBookNode.appendChild(newBookBtn);
+      importDiv.appendChild(newBookNode);
+    }
+  })
 }
+
+importBookBtn.onclick = () => {
+  ImportBook();
+}
+
+searchInput.onkeydown = (e) => {
+  if (e.key === "Enter") {
+    ImportBook();
+  }
+
+}
+
 
 span.onclick = function() {
   modal.style.display = "none";
@@ -25,3 +62,7 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// function clearPrevious() {
+
+// }
