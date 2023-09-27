0f0f3c5238d5047e1fc56c97406ad0220fca87d3
volantxs <volantxs@gmail.com>
2023-09-27 23:31:59 +0530

    added mouseenter function to show description of the book

--- public/js/modal.js
+++ public/js/modal.js
@@ -4,14 +4,34 @@ var importBookBtn = document.getElementById("addBook")
 var span = document.getElementsByClassName("close")[0];
 var searchInput = document.getElementById("searchBar")
 var importDiv = document.getElementById("importedBooks")
+var bookBtns = document.querySelectorAll(".btn-book")
+var imgBtn = document.querySelectorAll(".img-shadow")
+var tooltip = document.getElementById("tooltip");
 var query = ''
 var URL = ''
 var googleKey = 'AIzaSyBmSE_tFFrraFhotpwPc8Vc4zMFEHL9zN8'
+var bookName;
+var bookID;
 
 importOpenBtn.onclick = function() {
   modal.style.display = "flex";
 }
 
+bookBtns.forEach(occurence => {
+  occurence.addEventListener("mouseenter", (e) => {
+    bookName = e.target.innerHTML;
+    bookID = e.target.id;
+    getDescription();
+  })
+  occurence.addEventListener("mouseleave", (e) => {
+    tooltip.innerHTML = '';
+    tooltip.style.display ="none";
+  })
+})
+
+window.addEventListener("click", () => {
+  tooltip.style.display = 'none'
+})
 function ImportBook() {
   query = searchInput.value;
   searchInput.value = '';
@@ -26,7 +46,6 @@ function ImportBook() {
       // img element
       var newBookImg = document.createElement('img')
       newBookImg.src = data.items[0].volumeInfo.imageLinks.smallThumbnail;
-      console.log(data.items[0].volumeInfo.imageLinks.smallThumbnail)
       newBookImg.className = "img-fluid mx-auto d-block rounded m-3 img-shadow"
       newBookImg.style.width ="100px";
       newBookImg.style.height ="160px";
@@ -35,11 +54,31 @@ function ImportBook() {
       var newBookBtn = document.createElement('button');
       newBookBtn.className="btn btn-book"
       newBookBtn.innerHTML = data.items[0].volumeInfo.title;
+      newBookBtn.id = "bookID-" + (new Date()).getTime();
       newBookNode.appendChild(newBookBtn);
       importDiv.appendChild(newBookNode);
     }
   })
 }
+
+function getDescription() {
+  query = bookName;
+  tooltip.style.display = "block"
+  tooltip.innerHTML = "Loading..."
+  URL = 'https://www.googleapis.com/books/v1/volumes?q=' + query + '&key=AIzaSyBmSE_tFFrraFhotpwPc8Vc4zMFEHL9zN8';
+  $.ajax({
+    url: URL.toString(),
+    dataType: 'json',
+    success: (data) => {
+      console.log(data);
+      const getData = data.items[0].volumeInfo.description;
+      tooltip.innerHTML = getData;
+    }
+  })
+  bookName ='';
+}
+
+
 
 importBookBtn.onclick = () => {
   ImportBook();
